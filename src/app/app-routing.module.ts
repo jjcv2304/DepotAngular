ffe8dee7230cf295d5f473db0fd1e0d7ef737a38
Juan <jjcv2304@hotmail.com>
2017-09-30 21:42:57 +0200

    adding navigation to item and itemtype

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ItemComponent} from './item/item.component';
+import {ItemTypeComponent} from './item-type/item-type.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'item'},
+  {path: 'item', component: ItemComponent},
+  {path: 'itemType', component: ItemTypeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { ItemComponent } from './item/item.component';
 import { ItemTypeComponent } from './item-type/item-type.component';
 import { ShelfComponent } from './shelf/shelf.component';
