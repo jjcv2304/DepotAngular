0213c9e51283e8f3a7f676085c5d2dab19d76ac5
Juan <jjcv2304@hotmail.com>
2017-10-02 10:07:19 +0200

    adding nav-bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { ItemComponent } from './item/item.component';
 import { ItemTypeComponent } from './item-type/item-type.component';
 import { ShelfComponent } from './shelf/shelf.component';
 import { ShelfTypeComponent } from './shelf-type/shelf-type.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { ShelfTypeComponent } from './shelf-type/shelf-type.component';
     ItemComponent,
     ItemTypeComponent,
     ShelfComponent,
-    ShelfTypeComponent
+    ShelfTypeComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
