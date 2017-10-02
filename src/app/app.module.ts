f10422bbba2178ea218558e91856a58a8fce6abf
Juan <jjcv2304@hotmail.com>
2017-10-02 15:50:36 +0200

    getting last version back of tslint and dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { ItemComponent } from './item/item.component';
-import { ItemTypeComponent } from './item-type/item-type.component';
+import { ItemTypeComponent } from './item/item-type/item-type.component';
 import { ShelfComponent } from './shelf/shelf.component';
-import { ShelfTypeComponent } from './shelf-type/shelf-type.component';
+import { ShelfTypeComponent } from './shelf/shelf-type/shelf-type.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
